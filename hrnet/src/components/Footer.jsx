9d2113aee269aa5cc248ce0a7fdf2a12f38eb6ab
Valentin Pierre <valentin.pierre71@gmail.com>
2022-10-20 15:17:55 +0200

    Footer component

---
+++ hrnet/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <span>2022 HRNet | All Rigth Reserved</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

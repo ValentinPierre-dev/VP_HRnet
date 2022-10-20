34282a23fbf342038bb64a13a95a580c3c3585da
Valentin Pierre <valentin.pierre71@gmail.com>
2022-10-20 15:16:55 +0200

    Header component

---
+++ hrnet/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <header>
+      <h1>HR Net</h1>
+      <div>
+        {toggle ? (
+          <Link to="/" id="path" onClick={() => setToggle(false)}>
+            Home
+          </Link>
+        ) : (
+          <Link
+            to="/employee-list"
+            id="path"
+            onClick={() => setToggle(!toggle)}
+          >
+            View Current Employees
+          </Link>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
